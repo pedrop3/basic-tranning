d54baa79ced4bf7b688d905b6de251442ed5f602
= <=>
2023-10-05 21:23:22 -0300

    one more day

---
+++ ArrayIntegers.java
@@ -0,0 +1,49 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
+/**
+ *
+ * Given an array of integers, keep a total score based on the following:
+ *
+ * Add 1 point for every even number in the array
+ * Add 3 points for every odd number in the array, except for the number 5
+ * Add 5 points for every time you encounter a 5 in the array
+ **/
+public class ArrayIntegers {
+
+  public static void main(String[] argh) {
+    Integer[] input = { 1, 2, 3, 4, 5 };
+    Integer[] input2 = { 17,19,21 };
+
+    ArrayList<Integer> arrayList = new ArrayList<>(Arrays.asList(input2));
+
+    System.out.println(checkPoints(arrayList));
+
+  }
+
+  private final static Integer checkPoints(ArrayList<Integer> arrayList) {
+
+    int points = 0;
+    if (arrayList.isEmpty()) {
+      return points;
+    }
+
+
+    for (int number : arrayList) {
+      if (number % 2 != 0) {
+        
+        if (number == 5) {
+          points += 5;
+        } else {
+          points += 3;
+        }
+      } else {
+        points += 1;
+
+      }
+    }
+
+    return points;
+  }
+
+}
